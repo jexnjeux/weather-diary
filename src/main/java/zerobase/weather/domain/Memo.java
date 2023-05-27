d6c5d234667c452bf24172ed71523b7def48f957
jexnjeux <zieun.na@gmail.com>
2023-05-27 13:19:03 +0700

    feat: JPA 방식으로 데이터 저장

--- src/main/java/zerobase/weather/domain/Memo.java
+++ src/main/java/zerobase/weather/domain/Memo.java
@@ -5,11 +5,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity(name="memo")
 public class Memo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // mysql 이 만들어준 key 값을 가져옴
     private int id;
     private String text;
 
--- src/test/java/zerobase/weather/repository/JdbcMemoRepositoryTest.java
+++ src/test/java/zerobase/weather/repository/JdbcMemoRepositoryTest.java
@@ -1,4 +1,4 @@
-package zerobase.weather;
+package zerobase.weather.repository;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
