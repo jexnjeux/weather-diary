f87fce05f30ddff8734047ebcc201fbb1ad7d4c0
jexnjeux <zieun.na@gmail.com>
2023-05-27 15:21:57 +0700

    feat: 날씨 데이터 가져오기

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -3,19 +3,51 @@ package zerobase.weather.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDate;
 
 @Service
 public class DiaryService {
     @Value("${openweathermap.key}")
     private String apiKey;
+
     public void createDiary(LocalDate date, String text) {
-        getWeatherString();
+        // open weather map 에서 날씨 데이터 가져오기
+        String weatherData = getWeatherString();
+
+        // 받아온 날씨 json 파싱하기
+
+        // 파싱된 데이터 + 일기 값 db 에 넣기
     }
 
     private String getWeatherString() {
         String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=" + apiKey;
-        System.out.println(apiUrl);
-        return "";
+
+        try {
+
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection(); // 요청을 보낼 수 있는 httpconnection
+            connection.setRequestMethod("GET"); // 요청을 보냄
+            int responseCode = connection.getResponseCode(); // 응답 받음
+            BufferedReader br;
+            if (responseCode == 200) {
+                br = new BufferedReader(new InputStreamReader(connection.getInputStream())); // 응답 코드를 BufferedReader 에 넣음
+            } else {
+                br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+            }
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine); // response 객체게 api 호출한 결과값이 쌓임
+            }
+            br.close();
+            return response.toString();
+        } catch (Exception e) {
+            return "failed to get response";
+        }
+
     }
 }
